437259139a1eec54c1053ef3780ad159972f4ae4
Ferjusto <102192277+Ferjusto@users.noreply.github.com>
2023-04-20 19:55:18 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,14 @@
+function geraArrayDeUrl(arrayLinks){
+
+    return arrayLinks.map(objetoLink => Object.values(objetoLink).join())
+
+}
+
+function validaURLArray(arrayLinks){
+
+    return geraArrayDeUrl(arrayLinks)
+
+}
+
+
+export default validaURLArray;
